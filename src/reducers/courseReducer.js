d4d9e7a5486c4b7151407ecd3ba97e2ee31d9c1d
Volodymyr <volodymyr.spodaryk@gmail.com>
2016-08-05 18:11:17 +0300

    #9 - Async in Redux; added mocked data storage; used react-thunk; improved look&feel of courses list

--- src/reducers/courseReducer.js
+++ src/reducers/courseReducer.js
@@ -2,11 +2,8 @@ import * as types from '../actions/actionsTypes';
 
 export default function courseReducer(state = [], action) {
   switch(action.type) {
-    case types.CREATE_COURSE:
-      return [
-        ...state,  // TODO: investigate usage 3-fullstops
-        Object.assign({}, action.course)
-      ];
+    case types.LOAD_COURSES_SUCCESS:
+      return action.courses;
 
     default:
       return state;
