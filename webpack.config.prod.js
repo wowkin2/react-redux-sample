83d60db900478edb8432d5723c5c4fa270d59be7
Volodymyr <volodymyr.spodaryk@gmail.com>
2016-08-11 13:33:09 +0300

    #14 - Deployment (at last working version!)

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -10,7 +10,7 @@ const GLOBALS = {
 export default {
   debug: true,
   devtool: 'source-map', // more info:https://webpack.github.io/docs/build-performance.html#sourcemaps and https://webpack.github.io/docs/configuration.html#devtool
-  noInfo: true, // set to false to see a list of every file being bundled.
+  noInfo: false, // set to false to see a list of every file being bundled.
   entry: './src/index',
   target: 'web',
   output: {
